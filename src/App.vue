da1a633d05efba4d9f3d1abd8768b9fc17bcc23f
Robert Zehnder <robert@kisdigital.com>
2018-08-29 13:36:07 -0400

    Adding bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
-  <div id="app" class="is-fluid">
+  <div id="app">
 
-    <nav class="navbar is-link" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <router-link class="navbar-item" to="/">
-              <h6 class="title is-6 has-text-white">VUE-RPG</h6>
-            </router-link>
-
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
-        <div class="navbar-menu">
-            <div class="navbar-start">
-                <router-link class="navbar-item" to="/character">Character</router-link>
-                <router-link class="navbar-item" to="/combat">Combat</router-link>
-                <router-link class="navbar-item" to="/mobs">Mobs</router-link>
-                <router-link class="navbar-item" to="/about">About</router-link>
-            </div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
+        <router-link class="navbar-brand" to="/">VUE-RPG</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/character">Character</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/combat">Combat</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/mobs">Mobs</router-link>
+                </li>
+            </ul>
         </div>
     </nav>
+
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-    @import '../node_modules/bulma/bulma.sass';
+    @import '../node_modules/bootstrap/scss/bootstrap';
 </style>
